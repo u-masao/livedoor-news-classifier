27a72c6e203de246d5919c3ec51ea51c27b38602
u-masao <4973920+u-masao@users.noreply.github.com>
2024-08-07 21:47:42 +0900

    Bugfix, ベクトルの平均化を実装

--- src/encoder.py
+++ src/encoder.py
@@ -27,7 +27,7 @@ class LongTextEncoder:
             self.device = "cuda:0"
 
         self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
-        self.model = AutoModel.from_pretrained(model_name).to(self.device)
+        self.model = AutoModel.from_pretrained(self.model_name).to(self.device)
         self.splitter = SentenceTransformersTokenTextSplitter(
             model_name=self.model_name,
             chunk_overlap=self.chunk_overlap,
@@ -48,10 +48,18 @@ class LongTextEncoder:
         return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
     @torch.no_grad()
-    def encode(self, sentences: List[str], normalize: bool = False):
+    def encode(
+        self,
+        sentences: List[str],
+        normalize: bool = False,
+        combine_method: str = "mean",
+    ):
         """
         埋め込み表現を得る
         """
+
+        # init logger
+        logger = logging.getLogger(__name__)
 
         # Tokenize the input texts
         batch_dict = self.tokenizer(
@@ -70,13 +78,60 @@ class LongTextEncoder:
             outputs.last_hidden_state, batch_dict["attention_mask"]
         )
 
-        # normalize embeddings
+        logger.debug(f"average pool shape: {embeddings.shape}")
+        logger.debug(f"average pool norm: {torch.norm(embeddings, dim=-1)}")
+
+        # 合成
+        if combine_method == "mean":
+            # チャンク毎のベクトルを平均化
+            embeddings = embeddings.mean(dim=0, keepdim=True)
+            logger.debug(f"mean shape: {embeddings.shape}")
+            logger.debug(f"mean norm: {torch.norm(embeddings, dim=-1)}")
+        elif combine_method == "weighted":
+            # チャンク毎のベクトルをトークン数で重み付けして平均化
+
+            # トークン数を取得
+            token_counts = batch_dict["attention_mask"].sum(axis=-1)
+            token_sum = token_counts.sum()
+
+            # 重み付け
+            embeddings = (embeddings.T * token_counts).T
+            logger.debug(f"weighted shape: {embeddings.shape}")
+            logger.debug(f"weighted norm: {torch.norm(embeddings, dim=-1)}")
+
+            # 平均化
+            if embeddings.dim() > 1:
+                embeddings = embeddings.sum(dim=0, keepdim=True) / token_sum
+            logger.debug(f"weighted sum shape: {embeddings.shape}")
+            logger.debug(
+                f"weighted sum norm: {torch.norm(embeddings, dim=-1)}"
+            )
+        else:
+            raise ValueError(
+                f"the combine_method not implemented: {combine_method}"
+            )
+
+        # ノーマライズ
         if normalize:
             embeddings = F.normalize(embeddings, p=2, dim=1)
+            logger.debug(f"normalized shape: {embeddings.shape}")
+            logger.debug(f"normalized norm: {torch.norm(embeddings, dim=-1)}")
 
+        # デバッグ出力
+        logger.debug(f"output shape: {embeddings.shape}")
+        logger.debug(f"output norm: {torch.norm(embeddings, dim=-1)}")
+        # シェイプを確認
+        assert embeddings.shape[0] == 1
+        assert embeddings.shape[1] == self.model.pooler.dense.out_features
         return embeddings
 
-    def split_and_embed(self, row: Tuple[str], prefix: str = "passage: "):
+    def split_and_encode(
+        self,
+        row: Tuple[str],
+        prefix: str = "passage: ",
+        normalize: bool = False,
+        combine_method: str = "mean",
+    ):
         """
         センテンスの分割と埋め込み処理
         """
@@ -93,8 +148,13 @@ class LongTextEncoder:
         text_chunks = [f"{prefix}{x}" for x in text_chunks]
 
         # encode
-        embeddings = torch.flatten(self.encode(text_chunks)).tolist()
+        embeddings = torch.flatten(
+            self.encode(
+                text_chunks, normalize=normalize, combine_method=combine_method
+            )
+        ).tolist()
 
+        # make metrics
         metrics = self.count_tokens(sentence, text_chunks)
         logger.info(metrics)
         return tuple([embeddings] + list(metrics.values()))
